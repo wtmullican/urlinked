58a93448d5959ae530d74228c4688cde72bc0c47
Terry Mullican <tmullican.tcs@gmail.com>
2019-06-22 00:55:16 -0500

    Create Select Resources.sql

---
+++ SQL_Scripts/Resources/Select Resources.sql
@@ -0,0 +1,7 @@
+SELECT *
+FROM dbo.Resources 
+JOIN dbo.ResourceLinks ON RSL_RS_SysID = RS_SysID
+JOIN dbo.Sections ON SCT_SysID = RSL_SCT_SysID
+JOIN dbo.SubCategories ON SCAT_SysID = SCT_SCAT_SysID
+JOIN dbo.Categories ON CAT_SysID = SCAT_CAT_SysID
+WHERE RS_SysID = 12450

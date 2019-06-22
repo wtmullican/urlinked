7110d0689eaef7b92e6a4eecddd9218ab002217e
Terry Mullican <tmullican.tcs@gmail.com>
2019-06-22 00:30:03 -0500

    Create Base Tables.sql

---
+++ SQL_Scripts/Base Tables.sql
@@ -0,0 +1,10 @@
+SELECT COUNT(*) FROM dbo.Resources (NOLOCK) 
+SELECT COUNT(*) FROM dbo.ResourceLinks (NOLOCK) 
+
+SELECT * FROM dbo.Tags (NOLOCK) 
+SELECT * FROM dbo.Categories (NOLOCK) 
+SELECT * FROM dbo.SubCategories (NOLOCK) 
+SELECT * FROM dbo.Sections (NOLOCK) 
+
+SELECT RS_DupCount, * FROM dbo.Resources (NOLOCK) 
+SELECT * FROM dbo.ResourceLinks (NOLOCK) 

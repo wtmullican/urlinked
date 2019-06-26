e639f9251590ab60e0901bc6fa3ef97a08798903
Terry Mullican <tmullican.tcs@gmail.com>
2019-06-25 20:53:32 -0500

    Update Base Tables.sql

--- SQL_Scripts/Base Tables.sql
+++ SQL_Scripts/Base Tables.sql
@@ -1,6 +1,7 @@
 SELECT COUNT(*) FROM dbo.Resources (NOLOCK) 
 SELECT COUNT(*) FROM dbo.ResourceLinks (NOLOCK) 
 
+SELECT * FROM dbo.ResourceTypes (NOLOCK)
 SELECT * FROM dbo.Tags (NOLOCK) 
 SELECT * FROM dbo.Categories (NOLOCK) 
 SELECT * FROM dbo.SubCategories (NOLOCK) 

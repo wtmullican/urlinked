585791ce8aff7287afbdd29eb14b71f5ca8d14b9
Terry Mullican <tmullican.tcs@gmail.com>
2019-07-01 16:32:45 -0500

    Update Base Tables.sql

--- SQL_Scripts/Base Tables.sql
+++ SQL_Scripts/Base Tables.sql
@@ -1,15 +1,15 @@
 /*
-SELECT 'Categories', COUNT(*) FROM dbo.Categories (NOLOCK) 
-SELECT 'ResourceErrors', COUNT(*) FROM dbo.ResourceErrors (NOLOCK) 
-SELECT 'ResourceLinks', COUNT(*) FROM dbo.ResourceLinks (NOLOCK) 
 SELECT 'Resources', COUNT(*) FROM dbo.Resources (NOLOCK) 
+SELECT 'ResourceLinks', COUNT(*) FROM dbo.ResourceLinks (NOLOCK) 
+--SELECT 'ResourceErrors', COUNT(*) FROM dbo.ResourceErrors (NOLOCK) 
 SELECT 'ResourceTypes', COUNT(*) FROM dbo.ResourceTypes (NOLOCK) 
-SELECT 'SearchTerms', COUNT(*) FROM dbo.SearchTerms (NOLOCK) 
-SELECT 'Sections', COUNT(*) FROM dbo.Sections (NOLOCK) 
+SELECT 'Categories', COUNT(*) FROM dbo.Categories (NOLOCK) 
 SELECT 'SubCategories', COUNT(*) FROM dbo.SubCategories (NOLOCK) 
+--SELECT 'SearchTerms', COUNT(*) FROM dbo.SearchTerms (NOLOCK) 
+SELECT 'Sections', COUNT(*) FROM dbo.Sections (NOLOCK) 
 SELECT 'Tags', COUNT(*) FROM dbo.Tags (NOLOCK) 
-SELECT 'XML_Import', COUNT(*) FROM dbo.XML_Import (NOLOCK) 
-SELECT 'XMLAPI_Log', COUNT(*) FROM dbo.XMLAPI_Log (NOLOCK) 
+--SELECT 'XML_Import', COUNT(*) FROM dbo.XML_Import (NOLOCK) 
+--SELECT 'XMLAPI_Log', COUNT(*) FROM dbo.XMLAPI_Log (NOLOCK) 
 */
 
 SELECT * FROM dbo.ResourceTypes (NOLOCK)

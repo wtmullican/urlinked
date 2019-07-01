8d2e024934a8ddffab4a00c6e6cc11ad05119f36
Terry Mullican <tmullican.tcs@gmail.com>
2019-06-30 21:45:55 -0500

    Create 012 - Parse FavoriteFlag.sql

---
+++ SQL_Scripts/XML_Import/012 - Parse FavoriteFlag.sql
@@ -0,0 +1,14 @@
+
+EXEC dbo.RS_ParseLongDesc_Fav_SP
+
+-- Review any bad records
+SELECT
+	RS_SysID
+	, RS_FavoriteFlag
+	, RS_LongDesc2
+	, LEN(RS_LongDesc2) as LD_Length
+	, RS_LongDesc
+FROM dbo.Resources
+LEFT OUTER JOIN dbo.ResourceTypes (NOLOCK) ON RST_SysID = RS_RST_SysID
+WHERE 1=1 AND ISNULL(RS_LongDesc2,'') <> ''
+	AND RS_LongDesc LIKE '%*UR*%' AND RS_FavoriteFlag = 0

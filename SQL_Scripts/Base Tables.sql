6cef4c796bca6225876d336e7093caf3a00f1f93
Terry Mullican <tmullican.tcs@gmail.com>
2019-06-30 21:04:05 -0500

    Update Base Tables.sql

--- SQL_Scripts/Base Tables.sql
+++ SQL_Scripts/Base Tables.sql
@@ -1,5 +1,16 @@
-SELECT COUNT(*) FROM dbo.Resources (NOLOCK) 
+/*
+SELECT COUNT(*) FROM dbo.Categories (NOLOCK) 
+SELECT COUNT(*) FROM dbo.ResourceErrors (NOLOCK) 
 SELECT COUNT(*) FROM dbo.ResourceLinks (NOLOCK) 
+SELECT COUNT(*) FROM dbo.Resources (NOLOCK) 
+SELECT COUNT(*) FROM dbo.ResourceTypes (NOLOCK) 
+SELECT COUNT(*) FROM dbo.SearchTerms (NOLOCK) 
+SELECT COUNT(*) FROM dbo.Sections (NOLOCK) 
+SELECT COUNT(*) FROM dbo.SubCategories (NOLOCK) 
+SELECT COUNT(*) FROM dbo.Tags (NOLOCK) 
+SELECT COUNT(*) FROM dbo.XML_Import (NOLOCK) 
+SELECT COUNT(*) FROM dbo.XMLAPI_Log (NOLOCK) 
+*/
 
 SELECT * FROM dbo.ResourceTypes (NOLOCK)
 SELECT * FROM dbo.Tags (NOLOCK) 

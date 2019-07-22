be85c0d9687d6cbd687c7e374b968a01fdde0595
Terry Mullican <tmullican.tcs@gmail.com>
2019-07-21 20:17:10 -0500

    Update Duplicate URLs.sql

--- SQL_Scripts/Resources/Duplicate URLs.sql
+++ SQL_Scripts/Resources/Duplicate URLs.sql
@@ -2,3 +2,7 @@ SELECT RS_URL, COUNT(*)
 FROM dbo.Resources (NOLOCK) 
 GROUP BY RS_URL
 HAVING COUNT(*) > 1
+
+--	To Review each one, to the RS_URL in the LIKE statement
+SELECT * FROM dbo.Resources where rs_url like 'hhttp://www.drug-addiction-help-now.org/blog/2013/07/an-addicted-child-and-a-mothers-journey-peeling-the-onion/%'
+

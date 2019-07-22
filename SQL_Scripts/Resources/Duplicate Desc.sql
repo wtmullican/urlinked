56eaae509443998b461cca0808668fcbd42513ab
Terry Mullican <tmullican.tcs@gmail.com>
2019-07-21 20:15:31 -0500

    Update Duplicate Desc.sql

--- SQL_Scripts/Resources/Duplicate Desc.sql
+++ SQL_Scripts/Resources/Duplicate Desc.sql
@@ -1,4 +1,9 @@
 SELECT RS_Desc, COUNT(*)
 FROM dbo.Resources (NOLOCK) 
+WHERE RS_Desc NOT IN ('Website')
 GROUP BY RS_Desc
 HAVING COUNT(*) > 1
+
+--	To Review each one, to the RS_Desc in the LIKE statement
+SELECT * FROM dbo.Resources where rs_desc like '$1.4 Million an Hour: That’s How Much This Problem Costs Missouri%'
+

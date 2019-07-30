aea6590287b1216f8d2e90c53a78c35e3eeb125d
Terry Mullican <tmullican.tcs@gmail.com>
2019-07-30 16:56:42 -0500

    Update Base Tables.sql

--- SQL_Scripts/Base Tables.sql
+++ SQL_Scripts/Base Tables.sql
@@ -1,15 +1,17 @@
 /*
 SELECT 'Resources', COUNT(*) FROM dbo.Resources (NOLOCK) 
 SELECT 'ResourceLinks', COUNT(*) FROM dbo.ResourceLinks (NOLOCK) 
---SELECT 'ResourceErrors', COUNT(*) FROM dbo.ResourceErrors (NOLOCK) 
+SELECT 'ResourceErrors', COUNT(*) FROM dbo.ResourceErrors (NOLOCK) 
 SELECT 'ResourceTypes', COUNT(*) FROM dbo.ResourceTypes (NOLOCK) 
 SELECT 'Categories', COUNT(*) FROM dbo.Categories (NOLOCK) 
 SELECT 'SubCategories', COUNT(*) FROM dbo.SubCategories (NOLOCK) 
 --SELECT 'SearchTerms', COUNT(*) FROM dbo.SearchTerms (NOLOCK) 
 SELECT 'Sections', COUNT(*) FROM dbo.Sections (NOLOCK) 
+SELECT 'Authors', COUNT(*) FROM dbo.Authors (NOLOCK) 
+SELECT 'Sources', COUNT(*) FROM dbo.Sources (NOLOCK) 
 SELECT 'Tags', COUNT(*) FROM dbo.Tags (NOLOCK) 
---SELECT 'XML_Import', COUNT(*) FROM dbo.XML_Import (NOLOCK) 
---SELECT 'XMLAPI_Log', COUNT(*) FROM dbo.XMLAPI_Log (NOLOCK) 
+SELECT 'XML_Import', COUNT(*) FROM dbo.XML_Import (NOLOCK) 
+SELECT 'XMLAPI_Log', COUNT(*) FROM dbo.XMLAPI_Log (NOLOCK) 
 */
 
 SELECT * FROM dbo.ResourceTypes (NOLOCK)
